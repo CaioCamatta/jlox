8aafdfec4c017a16568be234a797428e48a838e9
Caio Camatta <ccoelho6@uwo.ca>
2022-12-10 14:50:52 -0500

    Add structure, section 4.2 done

---
+++ src/com/camatta/lox/Token.java
@@ -0,0 +1,19 @@
+package com.camatta.lox;
+
+class Token {
+    final TokenType type;
+    final String lexeme; // A blob of characters
+    final Object literal;
+    final int line; // Note which line the token appears on (for error-handling). This could be more sophisticated
+    
+    Token(TokenType type, String lexeme, Object literal, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+}
