155337fef6a26fa401fb45d763005e6c3424f635
Caio Camatta <ccoelho6@uwo.ca>
2023-02-18 18:26:03 -0500

    Lint

--- src/com/camatta/lox/Stmt.java
+++ src/com/camatta/lox/Stmt.java
@@ -5,13 +5,21 @@ import java.util.List;
 abstract class Stmt {
     interface Visitor<R> {
         R visitBlockStmt(Block stmt);
+
         R visitClassStmt(Class stmt);
+
         R visitExpressionStmt(Expression stmt);
+
         R visitFunctionStmt(Function stmt);
+
         R visitIfStmt(If stmt);
+
         R visitPrintStmt(Print stmt);
+
         R visitReturnStmt(Return stmt);
+
         R visitVarStmt(Var stmt);
+
         R visitWhileStmt(While stmt);
     }
 
@@ -152,7 +160,5 @@ abstract class Stmt {
         final Stmt body;
     }
 
-
     abstract <R> R accept(Visitor<R> visitor);
 }
-
